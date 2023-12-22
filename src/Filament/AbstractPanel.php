50a10f327d7780c079b8a34d84043a81e01356ca
mohamedkhedr700 <M.khedr@rowaad.net>
2023-12-22 11:40:15 +0200

    Add filament package

--- src/Filament/AbstractPanel.php
+++ src/Filament/AbstractPanel.php
@@ -2,21 +2,8 @@
 
 namespace Raid\Core\Filament\Filament;
 
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
-use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Raid\Core\Filament\Traits\Panels\HasAuthMiddleware;
 use Raid\Core\Filament\Traits\Panels\HasColor;
 use Raid\Core\Filament\Traits\Panels\HasDiscover;
